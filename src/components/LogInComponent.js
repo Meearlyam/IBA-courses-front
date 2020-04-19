6a4df2572d38a68a79b6fa01b247e9738df749fd
Meearlyam <shavela99@gmail.com>
2020-04-20 01:00:45 +0300

    fill in LogInComponent

--- src/components/LogInComponent.js
+++ src/components/LogInComponent.js
@@ -1,9 +1,34 @@
 import React from 'react';
 
 class LogInComponent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: '',
+            password: ''
+        }
+    }
     render() {
         return(
-            <h1>Hello, I am Login!</h1>
+            <div>
+                <h1>Log in</h1>
+                <form>
+                    <label>Username:</label>
+                    <br/>
+                    <input className="textfield" type="text" onChange={(e, newUsername) =>
+                        this.setState({username:newUsername})}>
+                    </input>
+                    <br/>
+                    <label>Password: </label>
+                    <br/>
+                    <input className="textfield" type="password" onChange={(e, newPassword) =>
+                        this.setState({password:newPassword})}>
+                    </input>
+                    <br/><br/>
+                    <input className="button" type="submit" value="Log in"
+                           onClick={(e) => this.handleClick(e)}/>
+                </form>
+            </div>
         );
     }
 }
